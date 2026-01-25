38a68396d8acac4bc468db2db9d48f823c10cd30
Yudi Yamada <yudiyamada10@gmail.com>
2026-01-25 19:39:05 -0300

    chore: initial setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  color: red;
+`;
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Title>Hello World!</Title>
+      </main>
+    </div>
+  );
+}
