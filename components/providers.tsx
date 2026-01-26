d1187c63819e23effb704cb70b2f6ba1a236deb3
Yudi Yamada <yudiyamada10@gmail.com>
2026-01-25 22:40:44 -0300

    test: testing styled components

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "styled-components";
-import { theme } from "@/styles/theme"; 
+import { theme } from "@/styles/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -7,6 +7,8 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    // deixa a renderização das fontes mais nítida e suave
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
