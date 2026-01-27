d25492c254f10de5524a6561707ecc44e8e1dd6d
Yudi Yamada <yudiyamada10@gmail.com>
2026-01-27 18:36:26 -0300

    refactor: remove the comments

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,13 +1,27 @@
 export const theme = {
   colors: {
-    primary: "#FF8310",     // Laranja principal
-    background: "#191A20",  // O tom mais escuro da paleta
-    surface: "#232323",     // Cinza escuro (cards/inputs)
-    gray: "#393939",        // Cinza intermediário
-    lightGray: "#CCCCCC",   // Cinza claro (textos secundários)
+    primary: "#FF8310",
+    background: "#232323",
+    surface: "#191A20",
+    gray: "#393939",
+    lightGray: "#CCCCCC",
     white: "#FFFFFF",
   },
   borderRadius: "8px",
+  breakpoints: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    sl: "1280px",
+    xl: "1440px",
+  },
+  containers: {
+    sm: "600px",
+    md: "720px",
+    lg: "960x",
+    sl: "1200px",
+    xl: "1400px", 
+  }
 };
 
 export type ThemeType = typeof theme;
